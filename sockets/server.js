400a2ca93b75321cd868ab3eb3fd19d46b80004c
Richard Ding <rmding95>
2015-12-02 12:16:46 -0800

    wow

--- sockets/server.js
+++ sockets/server.js
@@ -3,3 +3,44 @@
     main server script for the socket.io chat demo
 */
 
+var net = require('net');
+var server = net.createServer();
+var clients = [];
+
+server.on('connection', function(socket) {
+    var name;
+
+    function broadcast(name, message) {
+        clients.forEach(function(client) {
+            if (client != socket) {
+                client.write('[' + name '] ' + message + '\n');
+            }
+        });
+    } 
+
+    clients.push(socket);
+
+    console.log('new connection');
+    socket.write('Hello what is your name?\n');
+
+    socket.on('data', function(data) {
+        if (!name) {
+            name = data.toString();
+            socket.write('Hi there ' + name + '\n');
+        } else {
+            broadcast(name, data.toString());
+        }
+    });
+
+    socket.on('close', function() {
+        console.log('connection closed');
+    });
+
+    
+});
+
+server.on('listening', function() {
+    console.log('server listening...');
+});
+
+server.listen(3000);
--- web-service/server.js
+++ web-service/server.js
@@ -3,3 +3,85 @@
     main server script for our task list web service
 */
 
+'use strict';
+
+var SQL_CREATE_TABLE = 'create table if not exists tasks(title string, done int, createdOn datetime)';
+
+var express = require('express');
+var sqlite = require('sqlite3');
+var bodyParser = require('body-parser');
+
+var app = express();
+var port = 8080;
+
+app.use(express.static(__dirname + '/static'));
+//tell express to parse post body data as json
+app.use(bodyParser.json());
+
+app.get('/api/tasks', function(req, res, next) {
+    db.all('select rowid, title, done, createdOn from tasks where done != 1', function(err, rows) {
+        if (err) {
+            return next(err);
+        }
+        //send data as json to client
+        res.json(rows);
+    });
+});
+//npm install --save body-parser, sqlite3, express
+app.post('/api/tasks', function(req, res, next) {
+    var newTask = {
+        title: req.body.title || 'New Task',
+        done: false,
+        createdOn: new Date()
+    };
+
+    var sql = 'insert into tasks(title, done, createdOn) values (?,?,?)';
+    db.run(sql, [newTask.title, newTask.done, newTask.createdOn], function(err) {
+        if (err) {
+            return next(err);
+        }
+        newTask.rowid = this.lastID;
+        res.status(201).location('/api/tasks/' + newTask.rowid).json(newTask);
+    });
+});
+
+app.put('/api/tasks/:rowid', function(req, res, next) {
+    var sql = 'update tasks set done=? where rowid=?';
+    db.run(sql, [req.body.done, req.params.rowid], function(err) {
+        if (err) {
+            return next(err);
+        }
+        res.json(req.body);
+    });
+});
+
+//global error handler for express
+app.use(function(err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).json({
+        message: err.message
+    });
+});
+    
+var db = new sqlite.Database(__dirname + '/data/tasks.db', function(err) {
+    if (err) {
+        throw err;
+    }
+
+    db.run(SQL_CREATE_TABLE, function(err) {
+        if (err) {
+            throw err;
+        }
+        app.listen(port, function() {
+            console.log('server is listening on http://localhost:' + port);
+        });
+    });
+});
+
+process.on('exit', function() {
+    if (db) {
+        console.log('closing the database');
+        db.close();
+    }
+});
+
